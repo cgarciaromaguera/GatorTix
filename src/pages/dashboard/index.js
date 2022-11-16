f72e15fa256e86f374341ab67b39ae6fba6bec40
Thomas Fai <tfai@ufl.edu>
2022-11-15 20:56:16 -0500

    removed dropdown components

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Button from 'components/Button'
-import Dropdown from 'components/Dropdown'
 import { actions } from 'slices/app.slice'
 import { images } from 'theme'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 import styles from './dashboard.module.scss'
-
 
 const Dashboard = () => {
   const dispatch = useDispatch()
   const { me } = useSelector((state) => state.app)
-
-  // Temporarily handling dropdown buttons
-  const handleMenuOne = () => {
-    // eslint-disable-next-line
-    console.log('clicked one');
-  };
-  const handleMenuTwo = () => {
-    // eslint-disable-next-line
-    console.log('clicked two');
-  };
 
   return (
     <div className={styles.root}>
@@ -54,20 +42,6 @@ const Dashboard = () => {
           {'\n'}
           for more information. */}
         </p>
-        <Dropdown
-          trigger={<button type="button">Dropdown 1</button>}
-          menu={[
-            <button type="button" onClick={handleMenuOne}>Menu 1</button>,
-            <button type="button" onClick={handleMenuTwo}>Menu 2</button>,
-          ]}
-        />
-        <Dropdown
-          trigger={<button type="button">Dropdown 2</button>}
-          menu={[
-            <button type="button" onClick={handleMenuOne}>Menu 3</button>,
-            <button type="button" onClick={handleMenuTwo}>Menu 4</button>,
-          ]}
-        />
         <div className={styles.buttonContainer}>
           <Button
             label="Logout"
