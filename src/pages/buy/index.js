c17ad2312aa794e7dc406834bc59c827d650c884
jonathanzhang53 <jonathanzhang53@gmail.com>
2022-12-05 15:50:58 -0500

    some ui improvements

--- src/pages/buy/index.js
+++ src/pages/buy/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from 'components/Button'
 import { useSelector } from 'react-redux'
 import styles from './buy.module.scss'
 
@@ -6,27 +7,30 @@ const Buy = () => {
   const { me } = useSelector((state) => state.app)
 
   return (
-    <div className="ticket-form">
-      <h1>UF Ticket Buyer</h1>
-      <form>
-        <fieldset>
-          <p>Full Name</p>
-          <input name="name" />
+    <div className={styles.root} style={{ height: '100vh' }}>
+      <div className="ticket-form text-center container">
+        <h1>UF Ticket Buyer</h1>
+        <form>
+          <fieldset>
+            <p>Full Name</p>
+            <input name="name" />
 
-          <p>E-mail</p>
-          <input name="game" />
+            <p>E-mail</p>
+            <input name="game" />
 
-          <p>Phone Number</p>
-          <input name="phone number" />
+            <p>Phone Number</p>
+            <input name="phone number" />
 
-          <p>Enter Ticket Price</p>
-          <input name="ticket price" />
+            <p>Enter Ticket Price</p>
+            <input name="ticket price" />
 
-          <p>Upload Ticket</p>
-          <input name="upload ticket" />
-        </fieldset>
-        <button type="submit">Submit</button>
-      </form>
+            <p>Upload Ticket</p>
+            <input name="upload ticket" />
+          </fieldset>
+          <hr />
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
     </div>
   )
 }
--- src/pages/navbar/index.js
+++ src/pages/navbar/index.js
@@ -25,7 +25,7 @@ const NavBar = () => {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">
+            <a className="nav-link" href="/report">
               Report
             </a>
           </li>
